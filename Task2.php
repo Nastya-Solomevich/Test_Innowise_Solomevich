6b182178eb2c04d498e6eb24b82a1ccbb8b4a784
Anastasia Solomevich <106804052+Nastya-Solomevich@users.noreply.github.com>
2022-07-05 03:26:05 +0300

    The solution of the Task 2 was added

--- Task2.php
+++ Task2.php
@@ -1,10 +1,24 @@
 <?php 
 
-namespace InnowiseTest\Task2;
+namespace Incurrent_dateiseTest\Task2;
+
 
 class Task2
 {
-    
+
+    public static function main(string $birthdayPerson) : string 
+    {
+        $current_date = date_create(date("d-m-Y"));
+        $array_date = explode(".", $birthdayPerson);
+        $new_birth_date = implode("-", $array_date);
+        $birth = date_create($new_birth_date);
+        $interval = date_diff($current_date, $birth);
+        
+        return $interval->format('Since '.$birthdayPerson.' have passed %R%a days');;
+    }
+
 }
+
+echo Task2 :: main("14.08.2002");
 
 ?>
