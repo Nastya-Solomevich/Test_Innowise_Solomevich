7b6604ec3c17147c5e835159afbb036b4c5992a3
Anastasia Solomevich <106804052+Nastya-Solomevich@users.noreply.github.com>
2022-07-11 22:28:46 +0300

    This solution was updated

--- Task1.php
+++ Task1.php
@@ -1,15 +1,23 @@
-<?php 
+<?php
 
 namespace Test_Innowise\Task1;
 
-class Task1 
+class Task1
 {
-    public static function main(int $degree) : string 
+    public static function weather(int $degree): string
     {
-        $testresult = ($degree<=10) ? "Temperature lower or equals than 10" : (($degree>10 && $degree<=20) ? "Temperature more than 10 degree" : (($degree>20 && $degree<=30) ? "Temperature more than 20 degree" : "Temperature more than 30 degree"));
+        $testresult = ($degree <= 10) ? "Temperature lower or equals than 10" : ((($degree > 10) && ($degree <= 20)) ? "Temperature more than 10 degree" : ((($degree > 20) && ($degree <= 30)) ? "Temperature more than 20 degree" : "Temperature more than 30 degree"));
 
         return $testresult;
     }
 }
+
+function main() 
+{
+    $degree = 18; 
+    echo Task1 :: weather($degree);
+}
+
+main();
 
 ?>
