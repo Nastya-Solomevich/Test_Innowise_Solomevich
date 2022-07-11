c6a9b39cf29ef3f1b53b5d36bb717896b037197b
Anastasia Solomevich <106804052+Nastya-Solomevich@users.noreply.github.com>
2022-07-11 22:34:07 +0300

    This solution was updated

--- Task2.php
+++ Task2.php
@@ -1,24 +1,29 @@
-<?php 
+<?php
 
 namespace Test_Innowise\Task2;;
 
-
 class Task2
 {
-
-    public static function main(string $birthdayPerson) : string 
+    public static function countOfDays(string $birthdayPerson): string
     {
         $current_date = date_create(date("d-m-Y"));
-        $array_date = explode(".", $birthdayPerson);
-        $new_birth_date = implode("-", $array_date);
-        $birth = date_create($new_birth_date);
-        $interval = date_diff($current_date, $birth);
-        
-        return $interval->format('Since '.$birthdayPerson.' have passed %R%a days');;
-    }
 
+        $array_date = explode(".", $birthdayPerson);
+
+        $new_birth_date = implode("-", $array_date);
+
+        $birth = date_create($new_birth_date);
+
+        $interval = date_diff($current_date, $birth);
+
+        return $interval->format('Since ' . $birthdayPerson . ' have passed %R%a days');;
+    }
 }
 
-echo Task2 :: main("14.08.2002");
+function main()
+{
+    $day = "14.08.2002";
+    echo Task2::countOfDays($day);
+}
 
-?>
+main();
