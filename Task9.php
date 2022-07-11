d8f09e861e7e16e4bb3d74295f39e4f312176c76
Anastasia Solomevich <106804052+Nastya-Solomevich@users.noreply.github.com>
2022-07-12 01:57:57 +0300

    Update #3

--- Task9.php
+++ Task9.php
@@ -1,55 +1,46 @@
-<?php 
+<?php
 
 namespace Test_Innowise\Task9;
 
-class Task9 
+class Task9
 {
-    public static function findTrio(array $arr, int $number) 
+    public static function findTrio(array $arr, int $number)
     {
-$arr = array_unique($arr);
+        $arr = array_unique($arr);
 
         sort($arr);
 
-        $new_arr = [];
+        $newArr = [];
 
-        $first=0;
+        $first = 0;
 
-        $last = count($arr)-1;
+        $last = count($arr) - 1;
 
-        while($first < $last) 
-        {
+        while ($first < $last) {
             $sum = $arr[$first] + $arr[$last] + $arr[$first + 1];
 
-            if($sum == $number)
-            {
-                array_push($new_arr, $arr[$first]."+".$arr[$last]."+".$arr[$first+1]."=".$number);
+            if ($sum == $number) {
+                array_push($newArr, $arr[$first] . "+" . $arr[$last] . "+" . $arr[$first + 1] . "=" . $number);
                 $first++;
                 $last--;
-            }
-            else 
-            {
-                if ($sum < $number) 
-                {
+            } else {
+                if ($sum < $number) {
                     $first++;
-                }
-                else
-                {
+                } else {
                     $last--;
                 }
             }
         }
 
-        return $new_arr;
-    }    
+        return $newArr;
+    }
 }
 
-function main() 
+function main()
 {
     $array = [1, 6, 5, 2, 7, 5, 1, 4, 3, 9, 8, 11, 10, 18, 12, 14, 13];
 
-    print_r(Task9 :: findTrio ($array, 18));
+    print_r(Task9::findTrio($array, 18));
 }
 
 main();
-
-?>
