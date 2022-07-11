76068e13b81a1e969632c984a3c3c364d05741ff
Anastasia Solomevich <106804052+Nastya-Solomevich@users.noreply.github.com>
2022-07-12 02:00:56 +0300

    Update #3

--- Task12.php
+++ Task12.php
@@ -2,58 +2,51 @@
 
 namespace Test_Innowise\Task12;
 
-    class MyCalculator 
+class MyCalculator
+{
+    protected $firstNumber;
+    protected $secondNumber;
+    protected $res;
+
+    function __construct(int $firstNumber, int $secondNumber)
     {
-        protected $firstNumber;
-        protected $secondNumber;
-        protected $res;
-
-        function __construct(int $firstNumber, int $secondNumber)
-        {
-            $this->firstNumber = $firstNumber;
-            $this->secondNumber = $secondNumber;
-            
-        }
-
-        public function add() : object 
-        {
-            $this->res = $this->firstNumber + $this->secondNumber;
-            return $this;
-        }
-
-        public function multiply() : object 
-        {
-            $this->res = $this->firstNumber * $this->secondNumber;
-            return $this;
-        }
-
-        public function divideBy(int $number) : int 
-        {
-            return $this->res = $this->res/$number;
-        }
-
-        public function substract() : object 
-        {
-            if ($this->firstNumber >= $this->secondNumber) 
-            {
-                $this->res = $this->firstNumber - $this->secondNumber;
-            }
-            else 
-            {
-                $this->res = $this->secondNumber - $this->firstNumber;
-            }
-            
-            return $this;
-        }
-
+        $this->firstNumber = $firstNumber;
+        $this->secondNumber = $secondNumber;
     }
 
-    function main() 
+    public function add(): object
     {
-        $mycalc = new MyCalculator(12, 6);
-        echo $mycalc->substract()->divideBy(3);
+        $this->res = $this->firstNumber + $this->secondNumber;
+        return $this;
     }
 
-    main();
+    public function multiply(): object
+    {
+        $this->res = $this->firstNumber * $this->secondNumber;
+        return $this;
+    }
 
-?>
+    public function divideBy(int $number): int
+    {
+        return $this->res = $this->res / $number;
+    }
+
+    public function substract(): object
+    {
+        if ($this->firstNumber >= $this->secondNumber) {
+            $this->res = $this->firstNumber - $this->secondNumber;
+        } else {
+            $this->res = $this->secondNumber - $this->firstNumber;
+        }
+
+        return $this;
+    }
+}
+
+function main()
+{
+    $mycalc = new MyCalculator(12, 6);
+    echo $mycalc->substract()->divideBy(3);
+}
+
+main();
