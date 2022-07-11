0fea41bef89d3cdf4156686dfbb86ba47e1684fc
Anastasia Solomevich <106804052+Nastya-Solomevich@users.noreply.github.com>
2022-07-11 22:49:44 +0300

    Update Task1.php

--- Task1.php
+++ Task1.php
@@ -6,8 +6,8 @@ class Task1
 {
     public static function weather(int $degree): string
     {
-        $testresult = ($degree <= 10) ? "Temperature lower or equals than 10" : ((($degree > 10) && ($degree <= 20)) ? "Temperature more than 10 degree" : ((($degree > 20) && ($degree <= 30)) ? "Temperature more than 20 degree" : "Temperature more than 30 degree"));
-        return $testresult;
+        $testResult = ($degree <= 10) ? "Temperature lower or equals than 10" : ((($degree > 10) && ($degree <= 20)) ? "Temperature more than 10 degree" : ((($degree > 20) && ($degree <= 30)) ? "Temperature more than 20 degree" : "Temperature more than 30 degree"));
+        return $testResult;
     }
 }
 
