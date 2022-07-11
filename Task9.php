f8f2e838ea94aa913a89ab78234713e83971b88a
Anastasia Solomevich <106804052+Nastya-Solomevich@users.noreply.github.com>
2022-07-11 21:58:38 +0300

    This solution was updated

--- Task9.php
+++ Task9.php
@@ -4,32 +4,52 @@ namespace Test_Innowise\Task9;
 
 class Task9 
 {
-    public static function main(array $arr, int $number) {
-        $arr = array_unique($arr);
+    public static function findTrio(array $arr, int $number) 
+    {
+$arr = array_unique($arr);
+
         sort($arr);
+
         $new_arr = [];
+
         $first=0;
+
         $last = count($arr)-1;
-        while($first < $last) {
-            $sum = $arr[$first]+$arr[$last]+$arr[$first+1];
-            if($sum == $number){
+
+        while($first < $last) 
+        {
+            $sum = $arr[$first] + $arr[$last] + $arr[$first + 1];
+
+            if($sum == $number)
+            {
                 array_push($new_arr, $arr[$first]."+".$arr[$last]."+".$arr[$first+1]."=".$number);
                 $first++;
                 $last--;
             }
-            else {
-                if ($sum < $number) {
+            else 
+            {
+                if ($sum < $number) 
+                {
                     $first++;
                 }
-                else{
+                else
+                {
                     $last--;
                 }
             }
         }
+
         return $new_arr;
     }    
 }
 
-print_r(Task9 :: main ([1, 6, 5, 2, 7, 5, 1, 4, 3, 9, 8, 11, 10, 18, 12, 14, 13], 18));
+function main() 
+{
+    $array = [1, 6, 5, 2, 7, 5, 1, 4, 3, 9, 8, 11, 10, 18, 12, 14, 13];
+
+    print_r(Task9 :: findTrio ($array, 18));
+}
+
+main();
 
 ?>
