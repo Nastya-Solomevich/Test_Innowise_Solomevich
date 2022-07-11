a11563db3b540a462dd7437d34d9687ae3b97bf7
Anastasia Solomevich <106804052+Nastya-Solomevich@users.noreply.github.com>
2022-07-11 22:42:21 +0300

    This solution was update

--- Task3.php
+++ Task3.php
@@ -2,27 +2,29 @@
 
 namespace Test_Innowise\Task3;
 
-class Task3 
+class Task3
 {
-    public static function main(int $number) 
+    public static function sumOfNumbers(int $number): string
     {
         $numberToString = strval($number);
         $arr = str_split($numberToString);
-        while (strlen($numberToString)!=1)
-        {
-            $pr_num = 0;
-            $numberToString = $numberToString-1;
-            foreach($arr as $value)
-            {
-                $pr_num+=intval($value);
-            }
-            $arr=str_split(strval($pr_num));
-        }
-        echo implode("",$arr);
 
-    } 
+        while (strlen($numberToString) != 1) {
+            $pr_num = 0;
+            $numberToString = $numberToString - 1;
+            foreach ($arr as $value) {
+                $pr_num += intval($value);
+            }
+            $arr = str_split(strval($pr_num));
+        }
+
+        return implode("", $arr);
+    }
 }
 
-Task3 :: main(20207070); 
+function main()
+{
+    echo Task3::sumOfNumbers(202070);
+}
 
-?>
+main();
