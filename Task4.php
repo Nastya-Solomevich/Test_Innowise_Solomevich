a4da3ba60f01a588b3b1a231a94dde76e87ee6d8
Anastasia Solomevich <106804052+Nastya-Solomevich@users.noreply.github.com>
2022-07-11 22:55:01 +0300

    Update #2

--- Task4.php
+++ Task4.php
@@ -2,24 +2,21 @@
 
 namespace Test_Innowise\Task4;
 
-class Task4 
+class Task4
 {
-    static public function delateSymbols(string $quote):string
+    static public function delateSymbols(string $quote): string
     {
         $arr1 = str_split($quote);
         $arr2 = [" ", "-", "_"];
         $result = array_diff($arr1, $arr2);
         return implode("", $result);
     }
-
 }
 
 function main()
 {
     $string = "The quick-brown_fox jumps over the_lazy-dog";
-    echo Task4 ::delateSymbols($string);
+    echo Task4::delateSymbols($string);
 }
 
 main();
-
-?>
